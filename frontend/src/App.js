c16100974ddac013735584c9bfd283206933c9cc
Amod Sachintha <amod077@gmail.com>
2019-01-27 23:08:51 +0530

    add and list blocks
    added axios, bootstrap 4.2 and reactstrap to ./frontent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
+import { Table, Button, Input, } from 'reactstrap';
 
 import './App.css';
 
@@ -13,12 +12,12 @@ class App extends Component {
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: this.generateBlockList(res) }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/blockchain/blocks');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -28,48 +27,75 @@ class App extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/api/world', {
+    if(this.state.post.trim() === ""){
+      this.setState({responseToPost: "Invalid"});
+      return;
+    }
+    const response = await fetch('/blockchain/mineBlock', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ post: this.state.post }),
+      body: JSON.stringify({ data: this.state.post }),
     });
-    const body = await response.text();
+    const body = await response.json();
 
-    this.setState({ responseToPost: body });
+    this.setState({
+        responseToPost: body.msg,
+    });
+
+    this.callApi()
+          .then(res => this.setState({ response: this.generateBlockList(res) }))
+          .catch(err => console.log(err));
+  };
+
+  generateBlockList = res =>{
+    console.log(res);
+      return Object.keys(res).map(function(key) {
+        let date = new Date(parseFloat(res[key]['timestamp'])*1000).toUTCString();
+          return <tr>
+            <th>{res[key]['index']}</th>
+            <td>{res[key]['hash'].slice(0,12).toUpperCase()}</td>
+            <td>{res[key]['data'] ? res[key]['data']: '-'}</td>
+            <td>{date}</td>
+          </tr>;
+      })
   };
 
 render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <div className={"container mt-3"}>
+
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <strong>Send data to Blockchain API:</strong>
+            </p>
+            <Input
+                type="text"
+                value={this.state.post}
+                onChange={e => this.setState({ post: e.target.value })}
+            />
+            <Button type="submit" className={"btn-success mt-2"}>Submit</Button>
+          </form>
+
+          <p className={"mb-2 mt-2"}>{this.state.responseToPost}</p>
+
+          <Table size={"sm"} responsive={true} striped={true} bordered={true}>
+            <thead>
+            <tr><th colSpan={"4"}>CHAIN</th></tr>
+            <tr>
+              <th>index</th>
+              <th>hash</th>
+              <th>data</th>
+              <th>timestamp</th>
+            </tr>
+            </thead>
+            <tbody>
+              {this.state.response}
+            </tbody>
+          </Table>
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +13,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+
+app.get('/blockchain/blocks',(req, res) => {
+  axios.get('http://localhost:4001/blocks')
+      .then(response => {
+        res.send(response.data)
+      })
+      .catch(error => {console.log('error: '+error)})
+});
+
+
+// Blockchain call for adding new block
+app.post('/blockchain/mineBlock',(req,res)=>{
+  // port 4001 runs the http api for the blockchain component.
+  axios.post('http://localhost:4001/mineBlock',{
+    data: req.body.data
+  }).then(response => {
+    console.log(response.data);
+  }).catch(error => {
+    console.log('error: '+ error);
+  });
+  res.json({msg: "Block Added!"})
+});
+
+
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
